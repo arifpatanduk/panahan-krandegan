8dc331c977d52c0fc434ff39b3dff35a9d624ec6
arifpatanduk <arifpatanduk2@gmail.com>
2021-11-13 00:36:56 +0700

    install laravel/ui auth

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomepageController::class, 'index'])->name('homepage');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
