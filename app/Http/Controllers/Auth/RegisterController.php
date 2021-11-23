98266b83bc8446a09c970a839a459eecda18a867
arifpatanduk <arifpatanduk2@gmail.com>
2021-11-23 23:33:13 +0700

    add ui-avatars when register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -68,6 +68,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'avatar' => 'https://ui-avatars.com/api/?name=' . $data['name'],
         ]);
 
         $user->assignRole('User');
