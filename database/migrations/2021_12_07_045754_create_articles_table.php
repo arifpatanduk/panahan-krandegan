4756c52714be1a8a4845798bdd060cd3e39a6191
arifpatanduk <arifpatanduk2@gmail.com>
2021-12-07 21:12:19 +0700

    create model & migration of article & categories

---
+++ database/migrations/2021_12_07_045754_create_articles_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('article_categories_id');
+            $table->unsignedBigInteger('admin_id');
+            $table->string('title');
+            $table->string('content');
+            $table->string('image');
+            $table->string('status');
+            $table->timestamps();
+
+            $table->foreign('article_categories_id')->references('id')->on('article_categories')->onDelete('cascade');
+            $table->foreign('admin_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
