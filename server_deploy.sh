ee95fe49f2689d2f519232cd670b03a8c3e45514
arifpatanduk <arifpatanduk2@gmail.com>
2021-12-09 18:11:27 +0700

    create github actions deployment

---
+++ server_deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -e
+
+echo "Deploying application ..."
+
+# Enter maintenance mode
+(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
+    # Update codebase
+    git fetch origin master
+    git reset --hard origin/master
+
+    # Install dependencies based on lock file
+    composer install --no-interaction --prefer-dist --optimize-autoloader
+
+    #npm install
+    #npm run development
+
+    # Note: If you're using queue workers, this is the place to restart them.
+    # ...
+
+    # Clear cache
+    php artisan optimize
+
+    # Reload PHP to update opcache
+
+# Exit maintenance mode
+php artisan up
+
+echo "Application deployed!"
