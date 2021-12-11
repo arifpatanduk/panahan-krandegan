1b1e56634341917b3d942245b9a7fd677997989d
arifpatanduk <arifpatanduk2@gmail.com>
2021-12-11 15:51:52 +0700

    create article done

--- resources/views/livewire/admin/article/article-list.blade.php
+++ resources/views/livewire/admin/article/article-list.blade.php
@@ -1,8 +1,16 @@
 <div>
-    <button class="my-2 btn btn-sm btn-primary">
+
+    @if (!$addMode)
+    <button wire:click.prevent="addArticle" class="my-2 btn btn-sm btn-primary">
         <span class="typcn typcn-document-add"></span>
         Buat Artikel
     </button>
+    @else
+    <div class="shadow-sm p-3 mb-5 bg-white rounded">
+        @include('livewire.admin.article.create-article')
+    </div>
+    <hr>
+    @endif
 
     <div class="table-responsive">
         <table class="table">
@@ -13,26 +21,32 @@
                 </tr>
             </thead>
             <tbody>
+                @foreach ($articles as $article)
                 <tr>
                     <td>
                         <div class="font-weight-bold mb-1">
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci ipsum dicta
-                            laboriosam, placeat corrupti nulla
+                            <h4>
+                                {{ $article->title }}
+                            </h4>
                         </div>
+                        <p>{{ $article->category->name }}</p>
                         <div>
-                            12-10-2021
+                            <span class="badge badge-sm rounded-pill bg-light">
+                                {{ $article->user->name }}
+                            </span>
+                            <span class="badge badge-sm badge-outline-light badge-pill text-dark">
+                                {{ $article->updated_at }}
+                            </span>
                         </div>
                     </td>
                     <td>
-                        <button class="btn btn-xs btn-warning mb-2">
-                            <span class="typcn typcn-pencil"></span>
-                            Edit
-                        </button>
-                        <button class="btn btn-xs btn-danger mb-2">
-                            <span class="typcn typcn-trash"></span> Hapus
+                        <button class="btn btn-sm btn-warning mb-2">
+                            <span class="typcn typcn-cog"></span>
+                            Kelola
                         </button>
                     </td>
                 </tr>
+                @endforeach
             </tbody>
         </table>
     </div>
